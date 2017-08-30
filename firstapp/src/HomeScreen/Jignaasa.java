2a4fd8f589fac61e91e11330f3a3c5cb41fab208
Sreenivasa SreekanthR <r.s.sreekanth@gmail.com>
2017-08-31 00:22:23 +0530

    Add a deco class to the project - this is used to add decorations in the output

--- firstapp/src/HomeScreen/Jignaasa.java
+++ firstapp/src/HomeScreen/Jignaasa.java
@@ -3,8 +3,12 @@ package HomeScreen;
 public class Jignaasa {
 
 	public static void main(String[] args) {
+		
+	deco a = new deco();
+	a.printstar(10);
      System.out.println("Hari Sarvottamma.. Vaayu Jivottamma...");
-
+     a.printline(10);
+  
 	}
 
 }
